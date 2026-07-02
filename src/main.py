7b78c81b34cca5410728c7a7b8705c1c16173c8f
Long <phamlogg@gmail.com>
2026-07-02 11:00:32 +0800

    feat: auto generate all md files to html files

--- src/main.py
+++ src/main.py
@@ -2,10 +2,12 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
-
+from generate_page import generate_page_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main() -> None:
@@ -15,6 +17,13 @@ def main() -> None:
 
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_page_recursive(
+            dir_path_content,
+            template_path,
+            dir_path_public
+    )
 
 
 main()
