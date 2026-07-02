249c4b963326b9a6c0d9c5ce1fa9bc6f0a0e9e7e
Long <phamlogg@gmail.com>
2026-07-02 11:50:53 +0800

    refactor: dir_path_public points to docs/

--- src/main.py
+++ src/main.py
@@ -6,9 +6,8 @@ from copystatic import copy_files_recursive
 from generate_page import generate_page_recursive
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_public = "./docs"
 dir_path_content = "./content"
-dir_path_docs = "./docs"
 template_path = "./template.html"
 
 
@@ -18,23 +17,18 @@ def main() -> None:
     else:
         basepath = '/'
 
-    # print("Deleting public directory...")
-    # if os.path.exists(dir_path_public):
-    #     shutil.rmtree(dir_path_public)
-    print("Deleting docs directory...")
-    if os.path.exists(dir_path_docs):
-        shutil.rmtree(dir_path_docs)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    # print("Copying static files to public directory...")
-    # copy_files_recursive(dir_path_static, dir_path_public)
-    print("Copying static files to docs directory...")
-    copy_files_recursive(dir_path_static, dir_path_docs)
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating page...")
     generate_page_recursive(
             dir_path_content,
             template_path,
-            dir_path_docs,
+            dir_path_public,
             basepath
     )
 
