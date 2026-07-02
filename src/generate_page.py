6194e9b0a62c7b8f695c308a6b694213ddff687d
Long <phamlogg@gmail.com>
2026-07-02 10:58:40 +0800

    add generate_page function

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,3 +1,6 @@
+from markdown_blocks import markdown_to_html_node
+import os
+
 def extract_title(md):
     md = md.split("\n")
     for line in md:
@@ -5,3 +8,25 @@ def extract_title(md):
             return line[2:]
 
     raise Exception("No title found")
+def generate_page(from_path, template_path, dest_path):
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
+
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
+
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
+
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
